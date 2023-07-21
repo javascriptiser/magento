6148f269e191a735e856c4308b0c48e2e1d711f2
woronetskianton <woronetskianton@gmail.com>
2023-07-21 13:07:19 +0300

    init

---
+++ etc/env.php
@@ -0,0 +1,82 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_1xie62'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'JzLvLjibxSTT3PjpTIFCBOn2cr26Eqs0'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '619_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '619_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '04ecd52981d72fb9eaa7a77d3ff8bb3d'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento',
+                'username' => 'magento',
+                'password' => 'magento',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'install' => [
+        'date' => 'Fri, 21 Jul 2023 09:57:46 +0000'
+    ]
+];
